1611bb312742bc6ffafff3b48af37c7e8235707b
Sachini <sachinideepasika.97@gmail.com>
2022-03-02 12:49:36 +0530

    give id, add uploadButton

--- src/components/FunctionBlock/index.tsx
+++ src/components/FunctionBlock/index.tsx
@@ -2,12 +2,10 @@ import { Row, Col } from "antd";
 import { withTranslation } from "react-i18next";
 import { Button } from "../../common/Button";
 import TextArea from "../../common/TextArea";
-// import { TextArea } from "../../common/TextArea";
-// import { FunctionBlockSection, Content, ContentWrapper } from "./styles";
 import { UploadIcon } from "../../common/UploadIcon";
 import { Fade } from "react-awesome-reveal";
 import {
-  LeftContentSection,
+  FunctionBlockSection,
   Content,
   ContentWrapper,
   ServiceWrapper,
@@ -21,6 +19,8 @@ interface FunctionBlockProps {
   content: string;
   image? :any;
   button: string;
+  uploadButton:  string;
+  id: string;
   t: any;
   originalImage?: string;
   originalLink?: string;
@@ -35,7 +35,10 @@ const FunctionBlock = ({
   content,
   image,
   button,
+  uploadButton,
+  id,
   t,
+
 }: FunctionBlockProps) => {
 
    const scrollTo = (id: string) => {
@@ -45,8 +48,10 @@ const FunctionBlock = ({
         });
    };
 
+
+
   return (
-    <LeftContentSection>
+    <FunctionBlockSection>
       <Fade direction="up">
         <Row justify="space-between" align="middle" >
             <h6>{t(title)}</h6>
@@ -55,6 +60,13 @@ const FunctionBlock = ({
                     <Content>{t(content)}</Content>
 
                     <UploadIcon src={image} width="50%" height="50%" />
+
+                     {uploadButton && (
+                           <Button name="submit" onClick={() => scrollTo("mission")}>
+                                {t(uploadButton)}
+                           </Button>
+                      )}
+
                      {button && (
                            <Button name="submit" onClick={() => scrollTo("mission")}>
                                 {t(button)}
@@ -74,93 +86,8 @@ const FunctionBlock = ({
             </Col>
         </Row>
       </Fade>
-    </LeftContentSection>
+    </FunctionBlockSection>
   );
 };
-
-
-
-
-// const FunctionBlock = ({ title, content, button, t }: FunctionBlockProps) => {
-//   const scrollTo = (id: string) => {
-//     const element = document.getElementById(id) as HTMLDivElement;
-//     element.scrollIntoView({
-//       behavior: "smooth",
-//     });
-//   };
-//   return (
-//     <FunctionBlockSection>
-//       <Slide direction="up">
-//         <Row justify="start" align="middle">
-//           <ContentWrapper>
-//             <Col lg={24} md={24} sm={24} xs={24}>
-//               <h6>{t(title)}</h6>
-//               <Content>{t(content)}</Content>
-//               {button && (
-//                 <Button name="submit" onClick={() => scrollTo("mission")}>
-//                   {t(button)}
-//                 </Button>
-//               )}
-//             </Col>
-//           </ContentWrapper>
-//         </Row>
-//          <Row justify="space-between" align="middle" id={id}>
-//                   <Col lg={11} md={11} sm={12} xs={24}>
-//                     <SvgIcon src={icon} width="100%" height="100%" />
-//                   </Col>
-//                   <Col lg={11} md={11} sm={11} xs={24}>
-//                     <ContentWrapper>
-//                       <h6>{t(title)}</h6>
-//                       <Content>{t(content)}</Content>
-//                       <ServiceWrapper>
-//                         <Row justify="space-between">
-//                           {typeof section === "object" &&
-//                             section.map((item: any, id: number) => {
-//                               return (
-//                                 <Col key={id} span={11}>
-//                                   <SvgIcon src={item.icon} width="60px" height="60px" />
-//                                   <MinTitle>{t(item.title)}</MinTitle>
-//                                   <MinPara>{t(item.content)}</MinPara>
-//                                 </Col>
-//                               );
-//                             })}
-//                         </Row>
-//                       </ServiceWrapper>
-//                     </ContentWrapper>
-//                   </Col>
-//                 </Row>
-//
-// {/*  right section   */}
-//
-//                  <Row justify="space-between" align="middle" id={id}>
-//                           <Col lg={11} md={11} sm={11} xs={24}>
-//                             <ContentWrapper>
-//                               <h6>{t(title)}</h6>
-//                               <Content>{t(content)}</Content>
-//                               <ButtonWrapper>
-//                                 {typeof button === "object" &&
-//                                   button.map((item: any, id: number) => {
-//                                     return (
-//                                       <Button
-//                                         key={id}
-//                                         color={item.color}
-//                                         fixedWidth={true}
-//                                         onClick={() => scrollTo("about")}
-//                                       >
-//                                         {t(item.title)}
-//                                       </Button>
-//                                     );
-//                                   })}
-//                               </ButtonWrapper>
-//                             </ContentWrapper>
-//                           </Col>
-//                           <Col lg={11} md={11} sm={12} xs={24}>
-//                             <SvgIcon src={icon} width="100%" height="100%" />
-//                           </Col>
-//                         </Row>
-//       </Slide>
-//     </FunctionBlockSection>
-//   );
-// };
 
 export default withTranslation()(FunctionBlock);
